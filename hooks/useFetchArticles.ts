d7f4e4959d8bb32c66097247f593bbcb05646624
rizwan <rzzvaan@gmail.com>
2024-08-17 03:18:33 +0500

    chore: inital prototype for news implemented

---
+++ hooks/useFetchArticles.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import { fetchArticles } from '../services/apiService';
+
+export const useFetchArticles = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const getArticles = async () => {
+      try {
+        const data = await fetchArticles();
+        setArticles(data);
+      } catch (error) {
+        console.error('Error fetching articles:', error);
+      }
+    };
+
+    getArticles();
+  }, []);
+
+  return {articles};
+};
---
+++ services/apiService.ts
@@ -0,0 +1,60 @@
+import axios from 'axios';
+
+export const fetchArticles = async () => {
+  const apiEndpoints = [
+    {
+      url: process.env.NEXT_PUBLIC_NEWS_API_ORG_BASE_URL,
+      params: {
+        q: 'tesla',
+        from: '2024-07-16',
+        sortBy: 'publishedAt',
+        apiKey: process.env.NEXT_PUBLIC_NEWS_API_KEY
+      }
+    },
+    {
+      url: process.env.NEXT_PUBLIC_NY_TIMES_BASE_URL,
+      params: {
+        q: 'election',
+        'api-key': process.env.NEXT_PUBLIC_NY_TIMES_API_KEY
+      }
+    },
+    {
+      url: process.env.NEXT_PUBLIC_GUARDIAN_APIS_BASE_URL,
+      params: {
+        page: 2,
+        q: 'debate',
+        'api-key': process.env.NEXT_PUBLIC_GUARDIAN_API_KEY
+      }
+    }
+  ];
+
+  const responses = await Promise.all(
+    apiEndpoints.map(endpoint => axios.get(endpoint.url, { params: endpoint.params }))
+  );
+
+  const combinedArticles = responses?.flatMap(response => {
+    if (response.data.articles) {
+      return response.data.articles;
+    } else if (response.data.response && response.data.response.results) {
+      return response.data.response.results.map(doc => ({
+        title: doc.webTitle,
+        description: '',
+        url: doc.webUrl,
+        publishedAt: doc.webPublicationDate,
+        urlToImage: ''
+      }));
+    } else if (response.data.response && response.data.response.docs) {
+      return response.data.response.docs.map(doc => ({
+        title: doc.headline.main,
+        description: doc.snippet,
+        url: doc.web_url,
+        publishedAt: doc.pub_date,
+        urlToImage: doc.multimedia.length > 0 ? `https://www.nytimes.com/${doc.multimedia[0].url}` : ''
+      }));
+    } else {
+      return [];
+    }
+  });
+
+  return combinedArticles;
+};
